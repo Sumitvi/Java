bf0ea5a43912f9230df2b7a492aeb2d578a2ce94
Sumit Vishwakarma <sv0042595@gmail.com>
2024-08-19 23:08:05 +0530

    Day 6

---
+++ Day6/Invertedstar.java
@@ -0,0 +1,14 @@
+public class Invertedstar {
+
+    public static void main(String[] args) {
+
+        for(int line=5;line>=0;line--){
+            for(int star=1;star<=line-1;star++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+    }
+    
+}

56a769235afef6da4719e3f4ad0e8c27dc6f1a12
Sumit Vishwakarma <sv0042595@gmail.com>
2024-10-08 23:37:08 +0530

    2D Matrix

--- 2D Arrays/Creation.java
+++ 2D Arrays/Creation.java
@@ -1,29 +1,32 @@
 import java.util.Scanner;
 
-public class Creation{
+public class Creation {
 
 
-    public static void main(String args[]){
 
-        int Matrix[][]= new int[3][3];
-        int n =Matrix.length , m=Matrix[0].length;
+    public static void main(String[] args) {
+        int arr [][] = new int [3][3];
+        int n = arr.length;
+        int m = arr[0].length;
 
         Scanner sc = new Scanner(System.in);
-
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                Matrix[i][j] = sc.nextInt();
+
+                arr[i][j]=sc.nextInt();
+                
             }
         }
 
-        System.out.println("Array Is");
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-               System.out.print(Matrix[i][j] + " ");
+                System.out.print(arr[i][j]  +" ");
             }
             System.out.println();
-
         }
-
+        
+        
     }
+    
 }

ee5885e6310248fd09a33f1f8dda1b53cbcf11bd
Sumit Vishwakarma <sv0042595@gmail.com>
2024-10-29 23:36:10 +0530

    Bit_Manipulation

---
+++ BitWiseOperator/bit.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class bit{
+    public static void main(String args[]){
+
+        // Bitwise AND
+        System.out.println(5 & 6);
+
+        // BitWise OR
+        System.out.println(5 | 6);
+
+        // BitWise XOR
+
+        System.out.println(5 ^ 6);
+
+        // Binary One's Complement
+        System.out.println(~5 );
+
+
+        // Binary Left Shift <<
+
+        System.out.println(5<<2);
+
+    }
+}

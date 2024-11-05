83a178177fe016257057dc3827a77e39d63669e5
Sumit Vishwakarma <sv0042595@gmail.com>
2024-11-05 13:05:54 +0530

    Copy-Constructor

--- OOPS/copyconstructor.java
+++ OOPS/copyconstructor.java
@@ -1,35 +1,33 @@
-class Student {
+public class copyconstructor {
+
+    public static void main(String[] args) {
+        
+        Student s1 = new Student("Sumit", 20);
+        s1.display();
+
+        Student s2 = new Student(s1);
+        s2.display();
+
+    }
+}
+
+class Student{
     String name;
     int age;
 
-    // Parameterized constructor
-    public Student(String name, int age) {
+    Student(String name , int age){
         this.name = name;
         this.age = age;
     }
 
-    // Copy constructor
-    public Student(Student student) {
+    void display(){
+        System.out.println(name + " age is "+ age);
+    }
+
+    Student(Student student){
         this.name = student.name;
         this.age = student.age;
     }
 
-    // Method to display student details
-    public void display() {
-        System.out.println("Name: " + name + ", Age: " + age);
-    }
 }
 
-public class copyconstructor {
-    public static void main(String[] args) {
-        // Creating an object of Student
-        Student student1 = new Student("Alice", 20);
-
-        // Creating a copy of student1 using the copy constructor
-        Student student2 = new Student(student1);
-
-        // Displaying details of both students
-        student1.display();
-        student2.display();
-    }
-}

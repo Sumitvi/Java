9293c2f50c8c89a772a2f74289a1f44c9519f2a7
Sumit Vishwakarma <sv0042595@gmail.com>
2024-11-12 11:40:54 +0530

    new commit

--- Recursion/ArrayIsSorted.java
+++ Recursion/ArrayIsSorted.java
@@ -14,7 +14,7 @@ public class ArrayIsSorted {
     }
 
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,5,6,7};
+        int arr[]={1,2,3,4,6,5,7};
         int i = 0;
         System.out.println(isSorted(arr, i));
         
